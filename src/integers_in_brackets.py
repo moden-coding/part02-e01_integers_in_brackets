2cecf78c5a17a4bccdae0f3707fb8991fa4dcdd0
modengann <99417989+modengann@users.noreply.github.com>
2022-08-29 11:50:21 -0400

    Update integers_in_brackets.py

--- src/integers_in_brackets.py
+++ src/integers_in_brackets.py
@@ -3,12 +3,11 @@ import re
 
 def integers_in_brackets(s):
     
-    result = re.findall(r"\[\s*([\+\-]?\d*)\s*\]",s)
-    return [int(i) for i in result]
+    pass
     
 
 def main():
-    print(integers_in_brackets(" afd [asd] [12 ] [a34] [ -43 ]tt [+12]xxx"))
+    pass
 
 if __name__ == "__main__":
     main()
